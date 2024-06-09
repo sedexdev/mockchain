615cc05b471e47c4a462c13049a043b5f6df1491
Andrew Macmillan <andrew.macmillan@ymail.com>
2024-06-09 22:43:29 +0100

    move constant values to separate module

---
+++ src/mods/constants.rs
@@ -0,0 +1,5 @@
+// file paths
+pub const BLOCKCHAIN_PATH: &str = "./src/data/blockchain.json";
+pub const TRANSACTIONS_PATH: &str = "./src/data/transactions.json";
+pub const WALLETS_PATH: &str = "./src/data/wallets.json";
+pub const KEYPAIRS_PATH: &str = "./src/data/keypairs.json";
