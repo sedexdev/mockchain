89a97a8fb3e33f203bb3504bcd50378960c0c746
Andrew Macmillan <andrew.macmillan@ymail.com>
2024-06-14 09:46:36 +0100

    allow dead code for test constants

--- src/mods/constants.rs
+++ src/mods/constants.rs
@@ -5,10 +5,15 @@ pub const WALLETS_PATH: &str = "./src/data/wallets.json";
 pub const KEYPAIRS_PATH: &str = "./src/data/keypairs.json";
 
 // test file paths
+#[allow(dead_code)]
 pub const BLOCKCHAIN_PATH_TEST: &str = "./src/data/test_data/blockchain_test.json";
+#[allow(dead_code)]
 pub const TRANSACTIONS_PATH_TEST: &str = "./src/data/test_data/transactions_test.json";
+#[allow(dead_code)]
 pub const WALLETS_PATH_TEST: &str = "./src/data/test_data/wallets_test.json";
+#[allow(dead_code)]
 pub const KEYPAIRS_PATH_TEST: &str = "./src/data/test_data/keypairs_test.json";
 
 // hash delimiter
+#[allow(dead_code)]
 pub const DELIMITER: &str = "-%-";
