7a0bebaa497b5b858a55065e3a69dbe8e481af33
Andrew Macmillan <andrew.macmillan@ymail.com>
2024-07-01 21:00:51 +0100

    update logging to accept additional arguments - #33

--- src/mods/helpers.rs
+++ src/mods/helpers.rs
@@ -44,14 +44,14 @@ pub fn get_timestamp() -> String {
 /// Nothing
 pub fn create_wallet(name: String) {
     let key_pair = KeyPair::generate(name.clone());
-    Log::new(LogLevel::INFO, 6);
+    Log::new(LogLevel::INFO, 6, Some(vec![name.clone()]));
     let address = key_pair.public_key.clone();
     let wallet = Wallet {
-        name,
+        name: name.clone(),
         address,
         balance: 0,
     };
-    Log::new(LogLevel::INFO, 7);
+    Log::new(LogLevel::INFO, 7, Some(vec![name]));
     FileOps::write(KEYPAIRS_PATH.as_path(), "keypairs", key_pair);
     FileOps::write(WALLETS_PATH.as_path(), "wallets", wallet);
 }
@@ -71,7 +71,7 @@ pub fn create_wallet(name: String) {
 /// # Returns
 /// Nothing
 pub fn create_transaction(from: String, to: String, amount: i32) {
-    Log::new(LogLevel::INFO, 17);
+    Log::new(LogLevel::INFO, 19, None);
     // get wallet public keys
     let from_address = match Wallet::get_wallet_address(&from) {
         Some(key) => key.replace("\"", ""),
@@ -81,11 +81,11 @@ pub fn create_transaction(from: String, to: String, amount: i32) {
         Some(key) => key.replace("\"", ""),
         None => return,
     };
-    Log::new(LogLevel::INFO, 18);
+    Log::new(LogLevel::INFO, 20, Some(vec![from.clone(), to.clone()]));
 
     // get transaction hash
     let hash = hash_transaction(&from_address, &to_address, &amount.to_string());
-    Log::new(LogLevel::INFO, 19);
+    Log::new(LogLevel::INFO, 21, None);
 
     // get senders private key
     let mut json_data = FileOps::parse(KEYPAIRS_PATH.as_path());
@@ -106,11 +106,11 @@ pub fn create_transaction(from: String, to: String, amount: i32) {
             private_key.push_str(key_value);
         }
     }
-    Log::new(LogLevel::INFO, 20);
+    Log::new(LogLevel::INFO, 22, Some(vec![from.clone()]));
 
     // get the transaction signature and the signing key
     let (signature, signing_key) = KeyPair::sign(&hash, private_key);
-    Log::new(LogLevel::INFO, 21);
+    Log::new(LogLevel::INFO, 23, Some(vec![from.clone()]));
 
     let signing_data = Signing {
         name: from,
@@ -129,9 +129,9 @@ pub fn create_transaction(from: String, to: String, amount: i32) {
 
     // write objects to file
     FileOps::write(TRANSACTIONS_PATH.as_path(), "transactions", transaction);
-    Log::new(LogLevel::INFO, 23);
+    Log::new(LogLevel::INFO, 25, None);
     FileOps::write(SIGNING_DATA_PATH.as_path(), "signing_data", signing_data);
-    Log::new(LogLevel::INFO, 22);
+    Log::new(LogLevel::INFO, 24, None);
 }
 
 /// Mine the next block in the chain
@@ -147,7 +147,7 @@ pub fn create_transaction(from: String, to: String, amount: i32) {
 ///
 /// Nothing
 pub fn mine_block(name: String) {
-    Log::new(LogLevel::INFO, 8);
+    Log::new(LogLevel::INFO, 8, None);
     let mut base_data = FileOps::parse(BLOCKCHAIN_PATH.as_path());
     let blockchain = match base_data["blockchain"].as_array_mut() {
         Some(data) => data,
@@ -161,7 +161,11 @@ pub fn mine_block(name: String) {
     let mut base_data = FileOps::parse(TRANSACTIONS_PATH.as_path());
     // set mining difficulty
     let leading_zeros = String::from("0".repeat(2));
-    Log::new(LogLevel::INFO, 9);
+    Log::new(
+        LogLevel::INFO,
+        9,
+        Some(vec![leading_zeros.len().to_string()]),
+    );
     // get block hash
     let mut hash = hash_block(
         &nonce.to_string(),
@@ -178,14 +182,14 @@ pub fn mine_block(name: String) {
             &base_data.to_string(),
         );
     }
-    Log::new(LogLevel::INFO, 10);
+    Log::new(LogLevel::INFO, 10, None);
 
     // get the current timestamp
     let timestamp = get_timestamp();
 
     // get the merkle root of this Blocks Transactions
     let merkle_root = get_merkle_root(TRANSACTIONS_PATH.as_path());
-    Log::new(LogLevel::INFO, 11);
+    Log::new(LogLevel::INFO, 11, None);
 
     // pay all transactions
     let transactions = match base_data["transactions"].as_array_mut() {
@@ -202,13 +206,23 @@ pub fn mine_block(name: String) {
         };
 
         if t["from_address"] == "REWARD" {
+            Log::new(LogLevel::INFO, 12, Some(vec![t["to_address"].to_string()]));
             Wallet::update_balance(t["to_address"].to_string(), amount, "add");
         } else {
             Wallet::update_balance(t["to_address"].to_string(), amount, "add");
             Wallet::update_balance(t["from_address"].to_string(), amount, "subtract");
+            Log::new(
+                LogLevel::INFO,
+                13,
+                Some(vec![
+                    t["amount"].to_string(),
+                    t["from_address"].to_string(),
+                    t["to_address"].to_string(),
+                ]),
+            )
         }
     }
-    Log::new(LogLevel::INFO, 12);
+    Log::new(LogLevel::INFO, 14, None);
 
     let block = Block {
         timestamp,
@@ -220,12 +234,12 @@ pub fn mine_block(name: String) {
     };
 
     FileOps::write(BLOCKCHAIN_PATH.as_path(), "blockchain", block);
-    Log::new(LogLevel::INFO, 13);
+    Log::new(LogLevel::INFO, 15, None);
     Transaction::clear();
-    Log::new(LogLevel::INFO, 14);
+    Log::new(LogLevel::INFO, 16, None);
     Transaction::add_reward(name);
-    Log::new(LogLevel::INFO, 15);
-    Log::new(LogLevel::INFO, 16);
+    Log::new(LogLevel::INFO, 17, None);
+    Log::new(LogLevel::INFO, 18, None);
 }
 
 /// Verifies the integrity of the blockchain
@@ -241,7 +255,7 @@ pub fn mine_block(name: String) {
 /// bool
 /// ```
 pub fn verify_chain() -> bool {
-    Log::new(LogLevel::INFO, 24);
+    Log::new(LogLevel::INFO, 26, None);
     let mut bc_base_data = FileOps::parse(BLOCKCHAIN_PATH.as_path());
     let blockchain = match bc_base_data["blockchain"].as_array_mut() {
         Some(data) => data,
@@ -255,7 +269,11 @@ pub fn verify_chain() -> bool {
 
         // check hashes match for current record and previous block
         if current_block["previous_hash"] != previous_block["hash"] {
-            Log::new(LogLevel::ERROR, 25);
+            Log::new(
+                LogLevel::ERROR,
+                27,
+                Some(vec![current_block["timestamp"].to_string()]),
+            );
             return false;
         }
 
@@ -267,7 +285,11 @@ pub fn verify_chain() -> bool {
         );
 
         if current_block["hash"].to_string().replace("\"", "") != hash {
-            Log::new(LogLevel::ERROR, 25);
+            Log::new(
+                LogLevel::ERROR,
+                27,
+                Some(vec![current_block["timestamp"].to_string()]),
+            );
             return false;
         }
 
@@ -288,7 +310,7 @@ pub fn verify_chain() -> bool {
             );
 
             if transactions[j]["hash"] != t_hash.clone() {
-                Log::new(LogLevel::ERROR, 27);
+                Log::new(LogLevel::ERROR, 29, None);
                 return false;
             }
 
@@ -307,16 +329,16 @@ pub fn verify_chain() -> bool {
                         s["signing_key"].to_string().replace("\"", ""),
                     );
                     if !KeyPair::verify(signature, signing_key, t_hash.clone()) {
-                        Log::new(LogLevel::ERROR, 29);
+                        Log::new(LogLevel::ERROR, 31, Some(vec![s["name"].to_string()]));
                         return false;
                     }
                 }
             }
         }
     }
-    Log::new(LogLevel::INFO, 26);
-    Log::new(LogLevel::INFO, 28);
-    Log::new(LogLevel::INFO, 30);
-    Log::new(LogLevel::INFO, 31);
+    Log::new(LogLevel::INFO, 28, None);
+    Log::new(LogLevel::INFO, 30, None);
+    Log::new(LogLevel::INFO, 32, None);
+    Log::new(LogLevel::INFO, 33, None);
     true
 }
--- src/mods/log.rs
+++ src/mods/log.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 use phf::phf_map;
 
 // imports
-use super::helpers::get_timestamp;
+use super::{helpers::get_timestamp, messaging::replace};
 use crate::{LOG_FILE_PATH, LOG_PATH};
 
 static LOG_MESSAGE_MAP: phf::Map<u8, &str> = phf_map! {
@@ -16,32 +16,38 @@ static LOG_MESSAGE_MAP: phf::Map<u8, &str> = phf_map! {
     3u8 => "Genesis block mined and appended to blockchain",
     4u8 => "Application data reset; wallet and key pair data preserved",
     5u8 => "Application data reset; wallet and key pair data deleted",
-    6u8 => "New ECDSA key pair created and appended to 'keypairs.json'",
-    7u8 => "New wallet initialised and appended to 'wallets.json'",
+    6u8 => "New ECDSA key pair created for '{}' and appended to 'keypairs.json'",
+    7u8 => "New wallet initialised for '{}' and appended to 'wallets.json'",
     8u8 => "Mining new block...",
-    9u8 => "...mining started with a current difficulty of 2; block hash must start with 2 leading zeros",
+    9u8 => "...mining started with a current difficulty of {}; block hash must start with <difficulty> leading zeros",
     10u8 => "...SHA256 block hash computed",
     11u8 => "...SHA256 merkle root of block transactions computed",
-    12u8 => "...all pending transactions including rewards processed and paid",
-    13u8 => "...new block appended to blockchain successfully",
-    14u8 => "...transaction data cleared",
-    15u8 => "...reward transaction appended to 'transactions.json'",
-    16u8 => "...mining complete",
-    17u8 => "Processing new transaction...",
-    18u8 => "...read senders and recipients public keys to start transaction",
-    19u8 => "...SHA256 transaction computed",
-    20u8 => "...extracted senders private key",
-    21u8 => "...transaction signed with senders private key using ECDSA",
-    22u8 => "...signing data appended to 'signing.json'",
-    23u8 => "...new transaction appended to 'transactions.json'",
-    24u8 => "Starting blockchain verification...",
-    25u8 => "...bad SHA256 block hash in chain; this chain has been tampered with, verification failed",
-    26u8 => "...block hashing is consistent",
-    27u8 => "...bad SHA256 transaction hash in chain; this chain has been tampered with, verification failed",
-    28u8 => "...transaction hashing is consistent",
-    29u8 => "...bad transaction signature in chain; a transaction could not be verified using ECDSA verification, verification failed",
-    30u8 => "...transaction signatures are consistent",
-    31u8 => "...blockchain verification completed successfully",
+    12u8 => "...block mining reward of 50 tokens paid to {}",
+    13u8 => "...processing transaction; {} tokens sent from {} to {}",
+    14u8 => "...all pending transactions including rewards processed and paid",
+    15u8 => "...new block appended to blockchain successfully",
+    16u8 => "...transaction data cleared",
+    17u8 => "...new reward transaction appended to 'transactions.json'",
+    18u8 => "...mining complete",
+    19u8 => "Processing new transaction...",
+    20u8 => "...read {}'s and {}'s public keys to start transaction",
+    21u8 => "...SHA256 transaction computed",
+    22u8 => "...extracted {}'s private key",
+    23u8 => "...transaction signed with {}'s private key using ECDSA",
+    24u8 => "...signing data appended to 'signing.json'",
+    25u8 => "...new transaction appended to 'transactions.json'",
+    26u8 => "Starting blockchain verification...",
+    27u8 => "...bad SHA256 block hash in chain on or before {}; this chain has been tampered with, verification failed",
+    28u8 => "...block hashing is consistent",
+    29u8 => "...bad SHA256 transaction hash in chain; this chain has been tampered with, verification failed",
+    30u8 => "...transaction hashing is consistent",
+    31u8 => "...bad transaction signature in chain; a transaction signed by {} could not be verified using ECDSA verification, verification failed",
+    32u8 => "...transaction signatures are consistent",
+    33u8 => "...blockchain verification completed successfully",
+};
+
+static LOG_PANIC_MAP: phf::Map<u8, &str> = phf_map! {
+    1u8 => "Unable to parse genesis block transaction to json_serde {}: {}",
 };
 
 /// Log enum with log level states
@@ -87,17 +93,40 @@ impl Log {
     ///
     /// # Args
     /// ```
-    /// level: LogLevel   -> log level being recorded
-    /// message: msg_key   -> log entry message
+    /// level: LogLevel  -> log level being recorded
+    /// message: msg_key -> log entry message
+    /// args: Option<Vec<String>> -> optional args to include in the message
     /// ```
     ///
     /// # Returns
     /// Nothing
-    pub fn new(level: LogLevel, msg_key: u8) {
+    pub fn new(level: LogLevel, msg_key: u8, args: Option<Vec<String>>) {
         Log::record(&Log {
             level,
             timestamp: get_timestamp(),
-            message: Log::get_log_message(msg_key),
+            message: Log::get_log_message(msg_key, args, false),
+        });
+    }
+
+    /// New instance of Log
+    ///
+    /// # Visibility
+    /// public
+    ///
+    /// # Args
+    /// ```
+    /// level: LogLevel           -> log level being recorded
+    /// message: msg_key          -> log entry message
+    /// args: Option<Vec<String>> -> optional args to include in the message
+    /// ```
+    ///
+    /// # Returns
+    /// Nothing
+    pub fn new_panic(level: LogLevel, msg_key: u8, args: Option<Vec<String>>) {
+        Log::record(&Log {
+            level,
+            timestamp: get_timestamp(),
+            message: Log::get_log_message(msg_key, args, true),
         });
     }
 
@@ -162,26 +191,41 @@ impl Log {
         }
     }
 
-    /// Get a valid log message from a HashMap of
-    /// messages using <i8, String> as the key/value
+    /// Get a valid log message from a HashMap of messages
+    /// using <u8, String> as the key/value and an optional
+    /// Vec of arguments to show in the message
     ///
     /// # Visibility
     /// public
     ///
     /// # Args
     /// ```
-    /// key: u8 -> HashMap key to lookup
+    /// key: u8                   -> HashMap key to lookup
+    /// args: Option<Vec<String>> -> optional args to include in the message
+    /// panic_flag: bool          -> true if logging a panic, false otherwise
     /// ```
     ///
     /// # Returns
     /// ```
     /// String
     /// ```
-    pub fn get_log_message(key: u8) -> String {
-        if let Some(msg) = LOG_MESSAGE_MAP.get(&key) {
-            msg.to_string()
-        } else {
-            "** UNABLE TO PROVIDE ENTRY DETAILS **".to_string()
+    pub fn get_log_message(key: u8, args: Option<Vec<String>>, panic_flag: bool) -> String {
+        let none_log: String = "** UNABLE TO PROVIDE ENTRY DETAILS **".to_string();
+
+        let helper = |map: &phf::Map<u8, &str>| {
+            if let Some(msg) = map.get(&key) {
+                match args {
+                    Some(vec) => return replace(msg.to_string(), vec),
+                    None => return msg.to_string(),
+                }
+            } else {
+                none_log.clone()
+            }
+        };
+
+        match panic_flag {
+            true => helper(&LOG_PANIC_MAP),
+            false => helper(&LOG_MESSAGE_MAP),
         }
     }
 }
